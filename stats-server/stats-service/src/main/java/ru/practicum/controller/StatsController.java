079b74b1f07dc305d6b393c0328ba59e9db5ede8
Salaia <puma.hope@yandex.ru>
2023-07-23 12:43:27 +0300

    checkstyle

--- stats-server/stats-service/src/main/java/ru/practicum/controller/StatsController.java
+++ stats-server/stats-service/src/main/java/ru/practicum/controller/StatsController.java
@@ -6,7 +6,6 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.EndpointHit;
